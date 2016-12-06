19a52be9a483290394b0741b17d30cf2f4a7dad8
Yuchen Zhou <yuchenzhou@Yuchens-MacBook-Air.local>
2016-12-05 19:54:07 -0500

    Modify main.swift database and collection configuation

--- backend/Sources/main.swift
+++ backend/Sources/main.swift
@@ -12,33 +12,51 @@ import MongoKitten
 
 let databaseName = "veggies_info_db"
 
-let collectionName_1 = "veggies_info_collection"
-let fieldName_1 = "name"
-let fieldType_1 = FieldType.string
-let fieldName_2 = "team"
-let fieldType_2 = FieldType.string
-let fieldName_3 = "age"
-let fieldType_3 = FieldType.int32
+let collectionName_1 = "test_collection_1"
 let collectionConfig_1 = CollectionConfiguration(collectionName: collectionName_1)
-collectionConfig_1.addNewFieldType(fieldName: fieldName_1, type: fieldType_1)
-collectionConfig_1.addNewFieldType(fieldName: fieldName_2, type: fieldType_2)
-collectionConfig_1.addNewFieldType(fieldName: fieldName_3, type: fieldType_3)
 
-let collectionName_2 = "test_collection2"
+let collectionName_2 = "veggies_info_collection"
 let collectionConfig_2 = CollectionConfiguration(collectionName: collectionName_2)
+let fieldName_21 = "name"
+let fieldType_21 = FieldType.string
+let fieldName_22 = "netID"
+let fieldType_22 = FieldType.string
+let fieldName_23 = "gender"
+let fieldType_23 = FieldType.boolean
+let fieldName_24 = "team"
+let fieldType_24 = FieldType.string
+let fieldName_25 = "height"
+let fieldType_25 = FieldType.string
+let fieldName_26 = "city"
+let fieldType_26 = FieldType.string
+let fieldName_27 = "languages"
+let fieldType_27 = FieldType.array
+let fieldName_28 = "hobbies"
+let fieldType_28 = FieldType.array
+let fieldName_29 = "degree"
+let fieldType_29 = FieldType.string
+collectionConfig_2.addNewFieldType(fieldName: fieldName_21, type: fieldType_21)
+collectionConfig_2.addNewFieldType(fieldName: fieldName_22, type: fieldType_22)
+collectionConfig_2.addNewFieldType(fieldName: fieldName_23, type: fieldType_23)
+collectionConfig_2.addNewFieldType(fieldName: fieldName_24, type: fieldType_24)
+collectionConfig_2.addNewFieldType(fieldName: fieldName_25, type: fieldType_25)
+collectionConfig_2.addNewFieldType(fieldName: fieldName_26, type: fieldType_26)
+collectionConfig_2.addNewFieldType(fieldName: fieldName_27, type: fieldType_27)
+collectionConfig_2.addNewFieldType(fieldName: fieldName_28, type: fieldType_28)
+collectionConfig_2.addNewFieldType(fieldName: fieldName_29, type: fieldType_29)
 
 let collectionName_3 = "test_collection"
-let fieldName_3_1 = "id"
-let fieldType_3_1 = FieldType.string
-let fieldName_3_2 = "count"
-let fieldType_3_2 = FieldType.int32
 let collectionConfig_3 = CollectionConfiguration(collectionName: collectionName_3)
-collectionConfig_3.addNewFieldType(fieldName: fieldName_3_1, type: fieldType_3_1)
-collectionConfig_3.addNewFieldType(fieldName: fieldName_3_2, type: fieldType_3_2)
+let fieldName_31 = "id"
+let fieldType_31 = FieldType.string
+let fieldName_32 = "count"
+let fieldType_32 = FieldType.int32
+collectionConfig_3.addNewFieldType(fieldName: fieldName_31, type: fieldType_31)
+collectionConfig_3.addNewFieldType(fieldName: fieldName_32, type: fieldType_32)
 
 let dictionary = [databaseName: [collectionName_1: collectionConfig_1, collectionName_2: collectionConfig_2, collectionName_3: collectionConfig_3]]
 
-let dbConfig = DatabaseConfiguration(host: "54.91.189.221", port: UInt16(27017), username: nil, password: nil)
+let dbConfig = DatabaseConfiguration(host: "127.0.0.1", port: UInt16(27017), username: nil, password: nil)
 
 let flexwork = Flexwork(dbConfig, dictionary: dictionary)
 
